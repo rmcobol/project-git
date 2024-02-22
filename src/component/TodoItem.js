b9d9487ccb16950f38b9702dd8dbc1aeb39992e9
rmcobol <lstrmcobol123@naver.com>
2024-02-22 10:13:33 +0900

    첫번째 작업

---
+++ src/component/TodoItem.js
@@ -0,0 +1,32 @@
+import "./TodoItem.css";
+
+
+function TodoItem({id,content,isDone,createdDate,onUpdate,onDelete}) {
+    const onChangeCheckbox = () =>{
+        onUpdate(id);
+    };
+
+    const onClickDelete = () =>{
+        onDelete(id);
+    }
+
+    return (
+        <div className="TodoItem">
+            <div className="checkbox_col">
+                <input
+                    onChange={onChangeCheckbox} 
+                    checked={isDone} 
+                    type="checkbox" />
+            </div>
+            <div className="title_col">{content}</div>
+            <div className="date_col">
+                {new Date(createdDate).toLocaleDateString()}
+            </div>
+            <div className="btn_col">
+                <button onClick={onClickDelete}>삭제</button>
+            </div>
+        </div>
+    );
+};
+
+export default TodoItem;
